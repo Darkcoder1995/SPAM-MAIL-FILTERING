760be986a929c7fa0c5a0c2f14ee0505ee6789ca
Darkcoder1995 <54602006+Darkcoder1995@users.noreply.github.com>
2021-02-11 00:13:36 -0500

    Add files via upload

---
+++ mlclass-ex6/emailFeatures.m
@@ -0,0 +1,20 @@
+function x = emailFeatures(word_indices)
+
+ 
+
+n = 1899;
+
+
+x = zeros(n, 1);
+
+
+
+for i = word_indices
+  x(i) = 1;
+end
+
+
+% =========================================================================
+    
+
+end
